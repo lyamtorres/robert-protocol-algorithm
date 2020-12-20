c16d902881292d8e541687d76cbc6b02f19a1e16
lyamtorres <lyamtorres@hotmail.com>
2020-12-20 16:39:55 +0100

    Add deleteContact function

--- src/main.cpp
+++ src/main.cpp
@@ -11,20 +11,66 @@
 
 using namespace std;
 
+const int joursDuMois[12]
+    = { 31, 28, 31, 30, 31, 30, 
+       31, 31, 30, 31, 30, 31 };
+
 int main() {
     srand(time(0));
-    
-    const int seuil = 4;
 
-    Telephone premierTelephone;
-    premierTelephone = initialiserTelephone(5);
-    afficherListe(premierTelephone);
-    supprimerPseudoFin(premierTelephone);
-    supprimerPseudoFin(premierTelephone);
-    afficherListe(premierTelephone);
-    rechargerListe(premierTelephone, seuil);
-    afficherListe(premierTelephone);
-    
+/*     Telephone samsungAlice = initialiserTelephone(5);
+    Telephone iphoneCharles = initialiserTelephone(5);
+
+    afficherListe(samsungAlice);
+    afficherListe(iphoneCharles); */
+
+    ListeDeContacts contactsAlice;
+    Date maDate;
+    Date maDate2;
+    Date maDate3;
+    Date maDate4;
+
+    maDate.jour = 1;
+    maDate.mois = 1;
+    maDate.an = 2021;
+
+    maDate2.jour = 3;
+    maDate2.mois = 1;
+    maDate2.an = 2021;
+
+    maDate3.jour = 28;
+    maDate3.mois = 1;
+    maDate3.an = 2021;
+
+    maDate4.jour = 29;
+    maDate4.mois = 1;
+    maDate4.an = 2021;
+
+
+    contactsAlice.tete = nullptr;
+
+    integrerContacts(contactsAlice, maDate);
+    integrerContacts(contactsAlice, maDate2);
+    integrerContacts(contactsAlice, maDate3);
+
+    afficherContacts(contactsAlice);
+
+    supprimerContact(contactsAlice, maDate4);
+
+    afficherContacts(contactsAlice);
+
+/*     supprimerPlusieurs(contactsAlice, maDate4);
+    supprimerPlusieurs(contactsAlice, maDate4);
+    supprimerPlusieurs(contactsAlice, maDate4);
+    supprimerPlusieurs(contactsAlice, maDate4);
+
+    supprimerPlusieurs(contactsAlice, maDate4);
+    supprimerPlusieurs(contactsAlice, maDate4); */
+
+
+/*     supprimerContact(contactsAlice);
+    afficherContacts(contactsAlice); */
+
     return 0;
 }
 
@@ -129,3 +175,90 @@ string donnerPseudonyme() {
   }
   return pseudonyme;
 }
+
+void integrerContacts(ListeDeContacts &liste, Date dateDuJour) {
+    Contact nouveauContact;
+    //int contactsDuJour = rand() % 20;
+
+    for (int i = 0; i < 2; i++) {
+        nouveauContact.pseudo1 = donnerPseudonyme();
+        nouveauContact.pseudo2 = donnerPseudonyme();
+        nouveauContact.date = dateDuJour;
+        genererMaillonContact(liste, nouveauContact);
+    }
+}
+
+void genererMaillonContact(ListeDeContacts &liste, Contact cont) {
+    MaillonContact *nouveauMaillon;
+    
+    nouveauMaillon = new MaillonContact;
+    nouveauMaillon->contact = cont;
+    nouveauMaillon->suivant = nullptr;
+    insererMaillonContact(liste, nouveauMaillon);
+}
+
+void insererMaillonContact(ListeDeContacts &liste, MaillonContact *maille) {
+    MaillonContact *actuel; // actuel correspond à l'élément où l'on est positionés dans la liste
+
+    if (liste.tete == nullptr) {
+        liste.tete = maille;
+    } else {
+        actuel = liste.tete;
+        while (actuel->suivant != nullptr) {
+            actuel = actuel->suivant;
+        }
+        actuel->suivant = maille;
+    }
+}
+
+void supprimerContacts(ListeDeContacts &liste, Date dateDuJour) {
+    MaillonContact *aSupprimer;
+    int nombreJours = calculerDifference(liste.tete->contact.date, dateDuJour);
+    
+    do {
+        aSupprimer = liste.tete;
+        liste.tete = liste.tete->suivant;
+        delete aSupprimer;
+        nombreJours = calculerDifference(liste.tete->contact.date, dateDuJour);
+    } while (nombreJours > 14);
+
+}
+
+int calculerDifference(Date date1, Date date2) {
+    long int n1 = date1.an * 365 + date1.jour;
+
+    for (int i = 0; i < date1.mois - 1; i++)
+        n1 += joursDuMois[i];
+    n1 += nombrerAnneeBinaire(date1);
+ 
+    long int n2 = date2.an * 365 + date2.jour;
+
+    for (int i = 0; i < date2.mois - 1; i++)
+        n2 += joursDuMois[i];
+    n2 += nombrerAnneeBinaire(date2);
+ 
+    return (n2 - n1);
+}
+
+int nombrerAnneeBinaire(Date date) {
+    int annee = date.an;
+ 
+    if (date.mois <= 2)
+        annee--;
+
+    return annee / 4 - annee / 100 + annee / 400;
+}
+
+void afficherContacts(ListeDeContacts liste) {
+    MaillonContact *actuel = liste.tete;
+
+    cout << "Tête -> ";
+    while (actuel != nullptr) {
+        cout << actuel->contact.pseudo1 << " -> ";
+        actuel = actuel->suivant;
+    }
+    cout << "NULL" << endl;
+}
+
+
+// Commande pour compiler : g++ main.cpp -o main.exe
--- src/main.h
+++ src/main.h
@@ -10,13 +10,36 @@ struct Telephone {
     Pseudo *queue;
 };
 
+struct Date {
+    int jour;
+    int mois;
+    int an;
+};
+
+struct Contact {
+    std::string pseudo1;
+    std::string pseudo2;
+    Date date;
+};
+
+struct MaillonContact {
+    Contact contact;
+    MaillonContact *suivant;
+};
+
+struct ListeDeContacts {
+    MaillonContact *tete;
+};
+
+            // QUESTION 1
+
 // Précondition : memoire > 0
 Telephone initialiserTelephone(int memoire);
 
 void afficherListe(Telephone tel);
 
-// Précondition : tel.espaceOccupe < tel.capacite
-void rechargerListe(Telephone &tel, int seuil);
+// Précondition : tel.espaceOccupe < tel.capacite et tel.espaceOccupe < seuil 
+void rechargerListe(Telephone &tel);
 
 // Précondition : Le chaînage doit être initialisé
 void insererPseudoAvant(Telephone &tel);
@@ -31,4 +54,27 @@ void supprimerPseudoDebut(Telephone &tel);
 void supprimerPseudoFin(Telephone &tel);
 
 std::string donnerPseudonyme();
+
+
+            // QUESTION 2
+
+// Rôle : Ajoute les nouveaux contacts du jour dans la liste de contacts de l'utilisateur 
+void integrerContacts(ListeDeContacts &liste, Date dateDuJour);
+
+// Rôle : Crée un pointeur de type MaillonContact à partir d'un type Contact
+void genererMaillonContact(ListeDeContacts &liste, Contact cont);
+ 
+void insererMaillonContact(ListeDeContacts &liste, MaillonContact *maille);
+
+// Rôle : Supprime tous les contacts avec plus de 14 jours d'avoir été enregistrés
+// Précondition: La liste des contacts doit commencer par les contacts moins récents
+void supprimerContacts(ListeDeContacts &liste, Date dateDuJour);
+
+// Rôle : Calcule le nombre de jours totals entre 2 dates données
+int calculerDifference(Date date1, Date date2);
+
+// Rôle : Verifie si l'année actuelle doit être considérée comme année binaire
+int nombrerAnneeBinaire(Date date);
+
+void afficherContacts(ListeDeContacts liste);
 
